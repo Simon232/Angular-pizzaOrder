76104566690671ed7bb2d766d06d1ba3125e3c6e
Simon <simonzsolt23@gmail.com>
2017-09-27 13:29:35 +0200

    working-version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { AppComponent }  from './app.component';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule ({
+   imports:      [ BrowserModule,HttpModule],
+   declarations: [ AppComponent],
+   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
